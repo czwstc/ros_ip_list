5a95e63ce9a8e3672cb3178be852e23b1884dc65
ZweiChen <zweichan@outlook.com>
2023-07-16 14:38:08 +0800

    Create ros-gen-Google.sh

---
+++ ros-gen-Google.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+mkdir -p ./pbr
+cd ./pbr
+
+# GoogleIP
+wget --no-check-certificate -c -O goog.json https://www.gstatic.com/ipranges/goog.json
+jq -r '.prefixes[] | select(has("ipv4Prefix")) | .ipv4Prefix' goog.json | cut -d '"' -f 1 > google.txt
+{
+echo "/ip firewall address-list"
+
+for net in $(cat google.txt.txt) ; do
+  echo "add list=Google address=$net comment=GOOGLE_IP_LIST"
+done
+
+} > ../IMPORT_GOOGLE_IPLIST.rsc
+
+cd ..
+rm -rf ./pbr
