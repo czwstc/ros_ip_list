609a54e0c4a63a7567130211119d47c41796e5d2
ZweiChen <zweichan@outlook.com>
2025-01-31 11:15:34 +0800

    Update ros-gen-CN.sh

--- ros-gen-CN.sh
+++ ros-gen-CN.sh
@@ -3,8 +3,8 @@ mkdir -p ./pbr
 cd ./pbr
 
 # AS4809 BGP
-# wget --no-check-certificate -c -O CN.txt https://raw.githubusercontent.com/mayaxcn/china-ip-list/master/chnroute.txt
-wget --no-check-certificate -c -O CN.txt https://raw.githubusercontent.com/misakaio/chnroutes2/master/chnroutes.txt
+wget --no-check-certificate -c -O CN.txt https://raw.githubusercontent.com/mayaxcn/china-ip-list/master/chnroute.txt
+#wget --no-check-certificate -c -O CN.txt https://raw.githubusercontent.com/misakaio/chnroutes2/master/chnroutes.txt
 
 {
 echo "/ip firewall address-list"
