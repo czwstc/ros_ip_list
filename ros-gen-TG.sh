ca344f84ddd2a7a4112115441955cc26afcfee54
ZweiChen <zweichan@outlook.com>
2025-08-02 19:55:46 +0800

    Update ros-gen-TG.sh

--- ros-gen-TG.sh
+++ ros-gen-TG.sh
@@ -8,7 +8,11 @@ wget --no-check-certificate -c -O tg.txt https://core.telegram.org/resources/cid
 {
 echo "/ip firewall address-list"
 
-for net in $(grep -v '^#' tg.txt) ; do
+# for net in $(grep -v '^#' tg.txt) ; do
+#   echo "add list=TG-IP address=$net comment=Telegram_IP_LIST"
+# done
+# 只处理 IPv4（包含“.”的行，过滤掉IPv6）
+for net in $(grep -v '^#' tg.txt | grep '\.') ; do
   echo "add list=TG-IP address=$net comment=Telegram_IP_LIST"
 done
 
